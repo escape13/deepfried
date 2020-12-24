fix gradient

GradientDescent/descent.py
@@ -21,7 +21,7 @@ def output_formula(features, weights, bias):
     return sigmoid(np.dot(features, weights) + bias)
 
 def error_formula(y, output):
-    return -(y * np.log(output) + (1 - y) * np.log(1 - output))
+    return -y * np.log(output) - (1 - y) * np.log(1 - output)
 
 def update_weights(x, y, weights, bias, learnrate):
     output = output_formula(x, weights, bias)
@@ -30,7 +30,7 @@ def update_weights(x, y, weights, bias, learnrate):
     bias += learnrate * d_error
     return weights, bias
 
-np.random.seed(13)
+np.random.seed(44)
 
 epochs = 100
 learnrate = 0.01
@@ -44,8 +44,7 @@ def train(features, targets, epochs, learnrate, graph_lines=False):
 
     for e in range(epochs):
         for x, y in zip(features, targets):
-            output = output_formula(features, weights, bias)
-            error = error_formula(y, output)
+            output = output_formula(x, weights, bias)
             weights, bias = update_weights(x, y, weights, bias, learnrate)
 
         out = output_formula(features, weights, bias)
@@ -81,4 +80,4 @@ data = pd.read_csv('data.csv', header=None)
 x = np.array(data[[0, 1]])
 print(x)
 y = np.array(data[[2]])
-train(x, y, epochs, learnrate)
+train(x, y, epochs, learnrate, True)